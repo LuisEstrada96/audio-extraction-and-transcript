b4a3573f02df7be2b74a2619314bb9005a683923
Luis Estrada <luis.estrada961107@gmail.com>
2020-04-03 10:52:44 -0600

    Fisrt commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Install
+
+### Python
+Version: Python 3.7.6
+
+
+### Install requirements
+```sh
+$ pip3 install -r requirements.txt
+```
+
+# Development
+```sh
+$ python3 file_to_execute.py
+```
+
---
+++ audio_extraction.py
@@ -0,0 +1,10 @@
+import moviepy.editor
+
+def getAudio(url_video, url_audio):
+    video = moviepy.editor.VideoFileClip(url_video)
+    audio = video.audio
+    audio.write_audiofile(url_audio)
+
+video = "./assets/test1.mp4"
+audio = "./assets/test1.mp3"
+getAudio(video, audio)
